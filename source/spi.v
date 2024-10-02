16bfe0e964fa3144d34d58fa9003146d6407ef96
Matej Pevec <matej2000@gmail.com>
2024-10-02 18:39:12 +0200

    Optimize soft SPI

--- source/spi.v
+++ source/spi.v
@@ -27,97 +27,117 @@ module soft_spi_slave
 	output reg 					data_ready,
 	input      [data_width-1:0] data_in 		// Must be ready within 1 serial clock cycle
 );
-	// sync SCK to the FPGA clock using a 3-bit shift register
-	reg [2:0] sck_r;  always @(posedge clk) sck_r <= {sck_r[1:0], sck};
-	wire sck_risingedge = (sck_r[2:1]==2'b01);
-	wire sck_fallingedge = (sck_r[2:1]==2'b10);
-
-	// syncing NCS
-	reg [2:0] ncs_r;  always @(posedge clk) ncs_r <= {ncs_r[1:0], ncs};
-	wire ncs_active = ~ncs_r[1];  // SSEL is active low
-	wire ncs_startmessage = (ncs_r[2:1]==2'b10);  // message starts at falling edge
-	wire ncs_endmessage = (ncs_r[2:1]==2'b01);  // message stops at rising edge
+	// Sync SCK to the FPGA clock using a 3-bit shift register
+	reg sck_r;  always @(posedge clk) sck_r <= sck;
+	wire sck_risingedge = ({sck_r,sck}==2'b01);
+	wire sck_fallingedge = ({sck_r,sck}==2'b10);
 	
-	// syncing MOSI
-	reg [1:0] si_r;  always @(posedge clk) si_r <= {si_r[0], si};
-	wire si_data = si_r[1];
-	
-	// storage
+	// Counters
 	reg [counter_width:0] 		data_count = 0;
 	reg [data_counter_width:0] 	data_out_count = 0;
-	reg [msg_width-1:0]			data_reg = 0;
 	
-	// Connecting wires/regs with pins
-	//assign addr = data_reg[msg_width-1:msg_width-1-addr_width];
-	//assign data_out = data_reg[msg_width-1-addr_width-1:0];
+	// Data shift reg
+	reg [data_width-1:0]		data_reg = 0;
 	
+	// Incoming data shift reg
 	always @ (posedge clk) begin
-		if (rst || ~ncs_active) begin
-			// Reset outputs
-			so <= 0;
-			rw <= 0;
-			
+		if (rst || ncs) begin
 			// Reset counters
 			data_count <= 0;
-			data_out_count <= 0;
-			
-			// Reset signals
-			addr_ready <= 0;
-			data_ready <= 0;
-			
 			// Reset registers
 			data_reg <= 0;
-			addr <= 0;
-			data_out <= 0;
-		end else if(ncs_active) begin
+		end else if(~ncs) begin
 			if(sck_risingedge) begin
 				// Shifting into the data register
-				data_reg <= {data_reg[msg_width-2:0], si_data};
-				data_count <= data_count + 1'b1;
-				
+				data_reg <= {data_reg[data_width-2:0], si};
+				data_count <= data_count + 1;
+			end else if (sck_fallingedge && data_ready) begin
+				// Counter overflow, prepare for another data packet
+				// Reset counters
+				data_count <= 0;
+				// Reset registers
+				data_reg <= 0;
+			end
+		end
+	end
+	
+	// Address handling
+	always @ (posedge clk) begin
+		if (rst || ncs) begin
+			// Reset outputs
+			rw <= 0;
+			// Reset signals
+			addr_ready <= 0;
+			// Reset registers
+			addr <= 0;
+		end else if(~ncs) begin
+			if(sck_risingedge) begin
 				// Check the R/W bit
 				if (data_count == 0)
-					rw <= si_data;
+					rw <= si;
 				
 				// Check when the address portion is fully received and move it to the output
 				if (data_count == (addr_width + rw_bit) - 1) begin
-					addr <= {data_reg[addr_width-2:0], si_data};
+					addr <= {data_reg[addr_width-2:0], si};
 					addr_ready <= 1;
 				end
-				
+			end else if (sck_fallingedge && data_ready) begin
+				// Counter overflow, prepare for another data packet
+				// Reset outputs
+				rw <= 0;
+				// Reset signals
+				addr_ready <= 0;
+				// Reset registers
+				addr <= 0;
+			end
+		end
+	end
+	
+	// Data handling
+	always @ (posedge clk) begin
+		if (rst || ncs) begin
+			// Reset signals
+			data_ready <= 0;
+			// Reset registers
+			data_out <= 0;
+		end else if(~ncs) begin
+			if(sck_risingedge) begin
 				// Check when the entire message is received
 				if (data_count == (msg_width) - 1) begin
-					data_out <= {data_reg[data_width-2:0], si_data};
+					data_out <= {data_reg[data_width-2:0], si};
 					data_ready <= 1;
 				end
-			end else if (sck_fallingedge) begin
+			end else if (sck_fallingedge && data_ready) begin
 				// Counter overflow, prepare for another data packet
-				if (data_ready) begin
-					// Reset outputs
-					so <= 0;
-					rw <= 0;
-					
-					// Reset counters
-					data_count <= 0;
-					data_out_count <= 0;
-					
-					// Reset signals
-					addr_ready <= 0;
-					data_ready <= 0;
-					
-					// Reset registers
-					data_reg <= 0;
-					addr <= 0;
-					data_out <= 0;
-				end
-				
-				// Shift out the outgoing data
-				if (addr_ready && (data_out_count < data_width)) begin
-					so <= data_in[data_width - 1 - data_out_count];
-					data_out_count <= data_out_count + 1'b1;
-				end
+				// Reset signals
+				data_ready <= 0;
+				// Reset registers
+				data_out <= 0;
 			end
 		end
 	end
 
+	// Data out handling
+	always @ (posedge clk) begin
+		if (rst || ncs) begin
+			// Reset outputs
+			so <= 0;
+			// Reset counters
+			data_out_count <= 0;
+		end else if(~ncs && sck_fallingedge) begin
+			// Counter overflow, prepare for another data packet
+			if (data_ready) begin
+				// Reset outputs
+				so <= 0;
+				// Reset counters
+				data_out_count <= 0;
+			end
+			
+			// Shift out the outgoing data
+			if (addr_ready && (data_out_count < data_width)) begin
+				so <= data_in[data_width - 1 - data_out_count];
+				data_out_count <= data_out_count + 1;
+			end
+		end
+	end
 endmodule
